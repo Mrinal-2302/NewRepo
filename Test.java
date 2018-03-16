c5c88847d31176f96ec31b0837795b320518f89f
mrinal-2302 <mrinalbca.coc@gmail.com>
2018-03-16 13:26:53 +0530

    uuu

---
+++ Test.java
@@ -0,0 +1,12 @@
+package com.svn.test;
+
+public class Test {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Svn tested...");
+		System.out.println("Svn testeddd...");
+
+	}
+
+}
